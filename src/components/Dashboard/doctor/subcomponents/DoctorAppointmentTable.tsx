e3fb638d15cc88fa2fb0273788d9cf31fa968ec6
Dennis2340 <kamaradennis36@gmail.com>
2023-09-19 17:41:17 +0000

    with the table using a dummy data

--- src/components/Dashboard/doctor/subcomponents/DoctorAppointmentTable.tsx
+++ src/components/Dashboard/doctor/subcomponents/DoctorAppointmentTable.tsx
@@ -1,5 +1,4 @@
-"use client"
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -10,84 +9,214 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-  InputAdornment,
   IconButton,
-  InputLabel,
   Select,
   MenuItem,
-  SelectChangeEvent,
-  Modal,
-  Card,
-  Typography
+  FormControl,
+  Checkbox,
 } from "@mui/material";
-import { Delete, Edit, Add, Search,} from "@mui/icons-material";
-import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
-import CloseIcon from '@mui/icons-material/Close';
+import { Delete, Edit } from "@mui/icons-material";
 import Swal from "sweetalert2";
 
+// Dummy Data for Appointments
+const dummyAppointments = [
+    {
+      patient: {
+        patient: { name: "John Doe" },
+        user: { username: "johndoe" },
+        bloodGroup: "A+",
+      },
+      doctor: {
+        doctor: { name: "Dr. Smith" },
+        user: { username: "drsmith" },
+        specialization: "Cardiology",
+      },
+      appointment: {
+        date: "2023-09-20",
+        status: "pending",
+      },
+    },
+    {
+      patient: {
+        patient: { name: "Jane Doe" },
+        user: { username: "janedoe" },
+        bloodGroup: "B-",
+      },
+      doctor: {
+        doctor: { name: "Dr. Johnson" },
+        user: { username: "drjohnson" },
+        specialization: "Dermatology",
+      },
+      appointment: {
+        date: "2023-09-21",
+        status: "completed",
+      },
+    },
+    {
+      patient: {
+        patient: { name: "Alice Johnson" },
+        user: { username: "alicejohnson" },
+        bloodGroup: "AB+",
+      },
+      doctor: {
+        doctor: { name: "Dr. Brown" },
+        user: { username: "drbrown" },
+        specialization: "Orthopedics",
+      },
+      appointment: {
+        date: "2023-09-22",
+        status: "pending",
+      },
+    },
+    {
+      patient: {
+        patient: { name: "Bob Smith" },
+        user: { username: "bobsmith" },
+        bloodGroup: "O+",
+      },
+      doctor: {
+        doctor: { name: "Dr. Davis" },
+        user: { username: "drdavis" },
+        specialization: "Neurology",
+      },
+      appointment: {
+        date: "2023-09-23",
+        status: "completed",
+      },
+    },
+    {
+      patient: {
+        patient: { name: "Ella Williams" },
+        user: { username: "ellawilliams" },
+        bloodGroup: "A-",
+      },
+      doctor: {
+        doctor: { name: "Dr. Lee" },
+        user: { username: "drlee" },
+        specialization: "Pediatrics",
+      },
+      appointment: {
+        date: "2023-09-24",
+        status: "completed",
+      },
+    },
+    {
+      patient: {
+        patient: { name: "David Turner" },
+        user: { username: "davidturner" },
+        bloodGroup: "B+",
+      },
+      doctor: {
+        doctor: { name: "Dr. White" },
+        user: { username: "drwhite" },
+        specialization: "Gastroenterology",
+      },
+      appointment: {
+        date: "2023-09-25",
+        status: "pending",
+      },
+    },
+];
 
-type DoctorProfile = {
-    doctor:Doctor
-    user:User
-    specialization:Specialization
-}
+const DoctorAppointmentTable: React.FC = () => {
+  // State for filtering appointments
+  const [filter, setFilter] = useState("all");
+  // State for marking appointments as completed
+  const [completedAppointments, setCompletedAppointments] = useState<number[]>(
+    []
+  );
 
-type PatientProfile = {
-    patient:Patient
-    user:User
-    bloodGroup:BloodGroup
-}
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "center",
+    timer: 3000,
+    timerProgressBar: true,
+    showConfirmButton: false,
+  });
 
-type AppointmentDetail = {
-    doctor:DoctorProfile
-    patient:PatientProfile
-    appointment:Appointment
-}
+  // Function to handle filtering appointments
+  const handleFilterChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setFilter(event.target.value as string);
+  };
 
-interface DoctorAppointmentsTableProps {
-    appointments: AppointmentDetail[];
-    onRefetch: () => void;
-  }
+  // Function to mark an appointment as completed
+  const handleMarkCompleted = (appointmentIndex: number) => {
+    const updatedCompletedAppointments = [...completedAppointments];
+    updatedCompletedAppointments.push(appointmentIndex);
+    setCompletedAppointments(updatedCompletedAppointments);
+  };
 
-const DoctorAppointmentTable:React.FC = () => {
-//////// put states below here ////////////////
-
-
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'center',
-        timer: 3000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-        
-      })
-
-    return (
-        <Box>
-            <Box>
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Patient</TableCell>
-                        <TableCell>Appointment Status</TableCell>
-                        <TableCell>Appointment Date</TableCell>
-                        <TableCell>Actions</TableCell>
+  return (
+    <Box>
+      <Box>
+        <FormControl variant="outlined" sx={{ marginBottom: 2 }}>
+          <Select
+            value={filter}
+            onChange={handleFilterChange}
+            size="small" // Make the dropdown smaller
+            sx={{ minWidth: 120 }} // Set a minimum width
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="completed">Completed</MenuItem>
+            <MenuItem value="pending">Pending</MenuItem>
+          </Select>
+        </FormControl>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Patient</TableCell>
+                <TableCell>Appointment Status</TableCell>
+                <TableCell>Appointment Date</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dummyAppointments.map((appointment, index) => {
+                if (
+                  filter === "all" ||
+                  (filter === "completed" &&
+                    completedAppointments.includes(index)) ||
+                  (filter === "pending" && !completedAppointments.includes(index))
+                ) {
+                  return (
+                    <TableRow
+                      key={index}
+                      sx={{
+                        cursor: "pointer",
+                        "&:hover": { backgroundColor: "#f4f4f4" },
+                      }}
+                    >
+                      <TableCell>{appointment.patient.patient.name}</TableCell>
+                      <TableCell>{appointment.appointment.status}</TableCell>
+                      <TableCell>{appointment.appointment.date}</TableCell>
+                      <TableCell>
+                        {!completedAppointments.includes(index) && (
+                          <Checkbox
+                            checked={completedAppointments.includes(index)}
+                            onChange={() => handleMarkCompleted(index)}
+                            color="primary"
+                          />
+                        )}
+                        <IconButton>
+                          <Edit />
+                        </IconButton>
+                        <IconButton>
+                          <Delete />
+                        </IconButton>
+                      </TableCell>
                     </TableRow>
-                    </TableHead>
-                    <TableBody>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
+};
 
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            </Box>
-        </Box>
-    )
-}
-
-export default DoctorAppointmentTable
+export default DoctorAppointmentTable;

05da90f66c444a4c742d12c011d9670eeeed1dce
Dennis2340 <kamaradennis36@gmail.com>
2023-09-19 18:06:51 +0000

    final table design with dummy data

--- src/components/Dashboard/doctor/subcomponents/DoctorAppointmentTable.tsx
+++ src/components/Dashboard/doctor/subcomponents/DoctorAppointmentTable.tsx
@@ -14,11 +14,19 @@ import {
   MenuItem,
   FormControl,
   Checkbox,
+  Badge, // Add Badge component
+  Dialog, // Add Dialog component
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  SelectChangeEvent,
+  InputAdornment,
+  TextField, // Add TextField component for search
 } from "@mui/material";
-import { Delete, Edit } from "@mui/icons-material";
+import { Delete, Edit, Search } from "@mui/icons-material";
 import Swal from "sweetalert2";
 
-// Dummy Data for Appointments
+
 const dummyAppointments = [
     {
       patient: {
@@ -116,7 +124,7 @@ const dummyAppointments = [
         status: "pending",
       },
     },
-];
+  ];
 
 const DoctorAppointmentTable: React.FC = () => {
   // State for filtering appointments
@@ -124,6 +132,14 @@ const DoctorAppointmentTable: React.FC = () => {
   // State for marking appointments as completed
   const [completedAppointments, setCompletedAppointments] = useState<number[]>(
     []
+  );
+  // State for search input
+  const [searchText, setSearchText] = useState("");
+  // State for confirmation dialog
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  // State to track the appointment to delete
+  const [appointmentToDelete, setAppointmentToDelete] = useState<number | null>(
+    null
   );
 
   const Toast = Swal.mixin({
@@ -135,8 +151,8 @@ const DoctorAppointmentTable: React.FC = () => {
   });
 
   // Function to handle filtering appointments
-  const handleFilterChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setFilter(event.target.value as string);
+  const handleFilterChange = (event: SelectChangeEvent<string>) => {
+    setFilter(event.target.value);
   };
 
   // Function to mark an appointment as completed
@@ -146,75 +162,162 @@ const DoctorAppointmentTable: React.FC = () => {
     setCompletedAppointments(updatedCompletedAppointments);
   };
 
+  // Function to handle opening the delete confirmation dialog
+  const handleOpenDeleteDialog = (appointmentIndex: number) => {
+    setAppointmentToDelete(appointmentIndex);
+    setDeleteDialogOpen(true);
+  };
+
+  // Function to handle closing the delete confirmation dialog
+  const handleCloseDeleteDialog = () => {
+    setAppointmentToDelete(null);
+    setDeleteDialogOpen(false);
+  };
+
+  // Function to delete an appointment
+  const handleDeleteAppointment = () => {
+    if (appointmentToDelete !== null) {
+      // Remove the appointment from the list (you may want to use API calls or other state management)
+      // For now, we'll just log the deletion
+      console.log(`Deleted appointment at index: ${appointmentToDelete}`);
+      setDeleteDialogOpen(false);
+    }
+  };
+
+  // Function to handle searching appointments
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
   return (
     <Box>
-      <Box>
-        <FormControl variant="outlined" sx={{ marginBottom: 2 }}>
+      <Box sx={{ marginBottom: 2 }}>
+        <FormControl variant="outlined" sx={{ marginRight: 2 }}>
           <Select
             value={filter}
             onChange={handleFilterChange}
-            size="small" // Make the dropdown smaller
-            sx={{ minWidth: 120 }} // Set a minimum width
+            size="small"
+            sx={{ minWidth: 120 }}
           >
             <MenuItem value="all">All</MenuItem>
             <MenuItem value="completed">Completed</MenuItem>
             <MenuItem value="pending">Pending</MenuItem>
           </Select>
         </FormControl>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Patient</TableCell>
-                <TableCell>Appointment Status</TableCell>
-                <TableCell>Appointment Date</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {dummyAppointments.map((appointment, index) => {
-                if (
-                  filter === "all" ||
-                  (filter === "completed" &&
-                    completedAppointments.includes(index)) ||
-                  (filter === "pending" && !completedAppointments.includes(index))
-                ) {
-                  return (
-                    <TableRow
-                      key={index}
-                      sx={{
-                        cursor: "pointer",
-                        "&:hover": { backgroundColor: "#f4f4f4" },
-                      }}
-                    >
-                      <TableCell>{appointment.patient.patient.name}</TableCell>
-                      <TableCell>{appointment.appointment.status}</TableCell>
-                      <TableCell>{appointment.appointment.date}</TableCell>
-                      <TableCell>
-                        {!completedAppointments.includes(index) && (
-                          <Checkbox
-                            checked={completedAppointments.includes(index)}
-                            onChange={() => handleMarkCompleted(index)}
-                            color="primary"
-                          />
-                        )}
-                        <IconButton>
-                          <Edit />
-                        </IconButton>
-                        <IconButton>
-                          <Delete />
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  );
-                } else {
-                  return null;
-                }
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <TextField
+          label="Search"
+          variant="outlined"
+          size="small"
+          value={searchText}
+          onChange={handleSearch}
+          sx={{ minWidth: 200 }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="end">
+                <Search/> {/* Add the Search icon here */}
+              </InputAdornment>
+            ),
+          }}
+        />
       </Box>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Patient</TableCell>
+              <TableCell>Appointment Status</TableCell>
+              <TableCell>Appointment Date</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dummyAppointments.map((appointment, index) => {
+              // Filter based on selected status and search text
+              const isCompleted =
+                completedAppointments.includes(index) &&
+                appointment.appointment.status === "completed";
+              const isPending =
+                !completedAppointments.includes(index) &&
+                appointment.appointment.status === "pending";
+              const matchesSearch =
+                searchText === "" ||
+                appointment.patient.patient.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+
+              if (
+                (filter === "all" || (filter === "completed" && isCompleted) || (filter === "pending" && isPending)) &&
+                matchesSearch
+              ) {
+                return (
+                  <TableRow
+                    key={index}
+                    sx={{
+                      cursor: "pointer",
+                      "&:hover": { backgroundColor: "#f4f4f4" },
+                    }}
+                  >
+                    <TableCell>{appointment.patient.patient.name}</TableCell>
+                    <TableCell>
+                      <Badge
+                        color={isCompleted ? "success" : "warning"}
+                        variant="dot"
+                      >
+                        {appointment.appointment.status}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{appointment.appointment.date}</TableCell>
+                    <TableCell>
+                      {!completedAppointments.includes(index) && (
+                        <Checkbox
+                          checked={completedAppointments.includes(index)}
+                          onChange={() => handleMarkCompleted(index)}
+                          color="primary"
+                        />
+                      )}
+                      <IconButton>
+                        <Edit />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => handleOpenDeleteDialog(index)}
+                      >
+                        <Delete />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                );
+              } else {
+                return null;
+              }
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog
+        open={deleteDialogOpen}
+        onClose={handleCloseDeleteDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete Appointment?"}
+        </DialogTitle>
+        <DialogContent>
+          <p>Are you sure you want to delete this appointment?</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDeleteDialog} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleDeleteAppointment}
+            color="primary"
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
